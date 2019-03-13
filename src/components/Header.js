22d615d70efc53443116a1dfca93a5ef5e489fdc
andrejd1 <ngo.andrej@gmail.com>
2019-03-13 18:03:30 +0100

    - init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import BackgroundSlider from 'react-background-slider'
+
+import image1 from '../images/header-background_1.jpg'
+import image2 from '../images/header-background_.jpg'
+import image3 from '../images/header-background.jpg'
+import image4 from '../images/header-background_2.jpg'
+import image5 from '../images/header-background_3.jpg'
+import image6 from '../images/header-background_4.jpg'
+import image7 from '../images/header-background_5.jpg'
+import image8 from '../images/header-background_6.jpg'
+import image9 from '../images/header-background_7.jpg'
+import image10 from '../images/header-background_8.jpg'
+import image11 from '../images/header-background_9.jpg'
+
 export default class Header extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <React.Fragment>
-      
+        <BackgroundSlider images={[ image1, image2, image3, image4, image5, image6
+          , image7, image8, image9, image10, image11]} duration={2} transition={3} />
       <header id="home">
          <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
@@ -14,15 +29,15 @@ export default class Header extends Component {
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
             </ul>
          </nav>
 
          <div className="row banner">
+
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
+               <h1 className="responsive-headline">I am {resumeData.name}</h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{resumeData.roleDescription}
                </h3>
                <hr/>
                <ul className="social">
@@ -38,6 +53,7 @@ export default class Header extends Component {
                   }
                </ul>
             </div>
+
          </div>
 
          <p className="scrolldown">
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,30 +1,32 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "name": "Thien Long Ngo",
+    "role": "Frontend Developer",
+    "linkedinId":"ngoandrej",
+    "skypeid": "andrejd21",
+    "roleDescription": "I am newbie to frontend development and I love to learn about new technologies in general. " +
+      "I am also interested in android mobile development. In free time I like to taking photographs, listening to music, " +
+      "going to concerts and spending time with the precious ones.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/ngoandrej/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/andrejd1",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "url":"http://twitter.com/andrejd1",
           "className":"fa fa-twitter"
         }
       ],
     "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "address":"Prague, CZ",
+    "website":"https://andrejd1.github.io",
     "education":[
       {
         "UniversityName":"The LNM Insitute of Information Technology",
